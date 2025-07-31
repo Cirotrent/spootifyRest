9d15d55f695468b6da45817330d3573fd67254f8
CiroTrentacosti <c.trentacosti.esterno@almaviva.it>
2025-07-31 16:19:39 +0200

    test 2

--- src/main/java/it/prova/spootifyRest/web/dto/albumDTO/AlbumDTO.java
+++ src/main/java/it/prova/spootifyRest/web/dto/albumDTO/AlbumDTO.java
@@ -13,7 +13,7 @@ import it.prova.spootifyRest.web.dto.branoDTO.BranoDTO;
 import it.prova.spootifyRest.web.dto.riproduzioneDTO.RiproduzioneDTO;
 
 public class AlbumDTO {
-//test
+//test test test
 	private Long id;
 	private String titolo;
 	private String genere;
